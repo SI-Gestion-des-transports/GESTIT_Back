30c6ea9df1383251e98b61a5e332a768ceed7458
D-Azria <d.azria.pro@gmail.com>
2023-07-27 12:15:56 +0200

    Commit — Spring Initializr + Maven JWT & Springdoc

---
+++ src/main/java/fr/diginamic/gestit_back/GestitBackApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.gestit_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestitBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestitBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/diginamic/gestit_back/GestitBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.gestit_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestitBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
