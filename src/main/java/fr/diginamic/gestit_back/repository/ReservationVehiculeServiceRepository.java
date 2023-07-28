9f2aa65ee2f9373ab6c2640920a81e8112996e55
D-Azria <d.azria.pro@gmail.com>
2023-07-28 15:25:48 +0200

    ReservationVehiculeService : Repo, Srvc, Ctrlr, Dto

---
+++ src/main/java/fr/diginamic/gestit_back/repository/ReservationVehiculeServiceRepository.java
@@ -0,0 +1,9 @@
+package fr.diginamic.gestit_back.repository;
+
+import fr.diginamic.gestit_back.entites.ReservationVehiculeService;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationVehiculeServiceRepository extends JpaRepository<ReservationVehiculeService, Integer> {
+}
