3f03f26ecae51041bb3e794843613a573adb3700
D-Azria <d.azria.pro@gmail.com>
2023-07-28 13:44:18 +0200

    AbstractVehicule @MappedSuperclass

--- src/main/java/fr/diginamic/gestit_back/entites/AbstractVehicule.java
+++ src/main/java/fr/diginamic/gestit_back/entites/AbstractVehicule.java
@@ -1,15 +1,12 @@
 package fr.diginamic.gestit_back.entites;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity
+
 @Data
-@Inheritance(strategy = InheritanceType.JOINED)
-public abstract class AbstractVehicule extends AbstractBaseEntity {
+@MappedSuperclass
+public abstract class AbstractVehicule extends AbstractBaseEntity{
     private String immatriculation;
 
     private Integer nombreDePlaceDisponibles;
