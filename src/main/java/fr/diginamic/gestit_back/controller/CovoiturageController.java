a20cc80ab7bacdf0df075beef1bae61fe6bb483a
D-Azria <d.azria.pro@gmail.com>
2023-09-11 17:05:19 +0200

    Back fonctionnel

--- src/main/java/fr/diginamic/gestit_back/controller/CovoiturageController.java
+++ src/main/java/fr/diginamic/gestit_back/controller/CovoiturageController.java
@@ -80,7 +80,6 @@ public class CovoiturageController {
      * @param covoiturageDto Le DTO contenant les informations sur le covoiturage à créer.
      * @param httpHeaders    Les entêtes HTTP, contenant notamment le JWT pour l'authentification.
      * @return Une réponse contenant le covoiturage créé.
-     * @throws CovoiturageNotFoundException Si le covoiturage n'a pas été trouvé ou ne peut être créé pour une raison quelconque.
      */
     @PostMapping("/create")
     public ResponseEntity<List<CovoiturageDtoRecord>> creerCovoiturage(
@@ -97,68 +96,7 @@ public class CovoiturageController {
 
 
 
-    /* Fonctions utilitaires */
-    public static Covoiturage Covoiturage_instanceExample() {
-        Covoiturage covoiturage = new Covoiturage();
-        Commune commune = new Commune("Paris", 75000);
-        Adresse adresseDepart = new Adresse(26, "rue des Alouettes", commune);
-        Adresse adresseArrivee = new Adresse(32, "Bvd des Aubépines", commune);
 
-        Utilisateur conducteur = new Utilisateur();
-        LocalDate dateConducteur = LocalDate.of(2022, 4, 6);
-        List<Covoiturage> conducteurCovoituragesOrganises = new ArrayList<>();
-        List<Covoiturage> conducteurCovoituragesPassagers = new ArrayList<>();
-        List<String> roleConducteur = new ArrayList<>();
-        roleConducteur.add("COLLABORATEUR");
-        conducteur.setEmail("RonaldMerziner@gmail.com");
-        conducteur.setMotDePasse("4321");
-        conducteur.setNom("Merziner");
-        conducteur.setCovoituragesOrganises(conducteurCovoituragesOrganises);
-        conducteur.setCovoituragesPassagers(conducteurCovoituragesPassagers);
-        conducteur.setDateNonValide(dateConducteur);
-        conducteur.setRoles(roleConducteur);
-
-        Utilisateur passager = new Utilisateur();
-        LocalDate datePassager = LocalDate.of(2020, 1, 8);
-        List<Covoiturage> covoituragesOrganises = new ArrayList<>();
-        List<Covoiturage> covoituragesPassagers = new ArrayList<>();
-        List<String> rolePassager = new ArrayList<>();
-        rolePassager.add("COLLABORATEUR");
-        passager.setEmail("RonaldMerziner@gmail.com");
-        passager.setMotDePasse("4321");
-        passager.setNom("Merziner");
-        passager.setCovoituragesOrganises(covoituragesOrganises);
-        passager.setCovoituragesPassagers(covoituragesPassagers);
-        passager.setDateNonValide(datePassager);
-        passager.setRoles(rolePassager);
-        List<Utilisateur> passagersABord = new ArrayList<>();
-
-        /* A CORRIGER : CET APPEL POSE PROBLEME */
-        // passagersABord.add(conducteur);
-
-        VehiculePerso vehiculeConducteur = new VehiculePerso();
-        vehiculeConducteur.setImmatriculation("789-hu-78");
-        Modele modele = new Modele();
-        Marque marque = new Marque();
-        marque.setNom("Fiat");
-        modele.setNom("mini500");
-        modele.setMarque(marque);
-        vehiculeConducteur.setModele(modele);
-        vehiculeConducteur.setNombreDePlaceDisponibles(4);
-        vehiculeConducteur.setProprietaire(conducteur);
-
-        covoiturage.setDistanceKm(15);
-        covoiturage.setDureeTrajet(30);
-        covoiturage.setNombrePlacesRestantes(4);
-        covoiturage.setAdresseDepart(adresseDepart);
-        covoiturage.setAdresseArrivee(adresseArrivee);
-        covoiturage.setOrganisateur(conducteur);
-        covoiturage.setId(23);
-        covoiturage.setVehiculePerso(vehiculeConducteur);
-        covoiturage.setPassagers(passagersABord);
-
-        return covoiturage;
-    }
 
     @PostMapping
     public ResponseEntity<?> add(@RequestBody @Valid Covoiturage covoiturage) {
@@ -168,10 +106,11 @@ public class CovoiturageController {
         URI uri = URI.create("/covoiturages/" + covoiturageDto.getId());
         System.out.println();
         return ResponseEntity.created(uri).body(covoiturageDto);
-
     }
 
-    @GetMapping("/{id}")
+
+
+/*    @GetMapping("/{id}")
     public ResponseEntity<?> get(@PathVariable("id") Integer id) {
         System.out.println("GET id");
         try {
@@ -182,7 +121,20 @@ public class CovoiturageController {
             e.printStackTrace();
             return ResponseEntity.notFound().build();
         }
+    }*/
+    @GetMapping("/{id}")
+    public ResponseEntity<CovoiturageDtoRecord> get(@PathVariable("id") Integer id) {
+        try {
+            Covoiturage covoiturage = covoiturageService.get(id);
+            CovoiturageDtoRecord covoitDto= covoiturageService.changeToCovoitDto(covoiturage);
+            return ResponseEntity.status(200).body(covoitDto);
+        } catch (CovoiturageNotFoundException e) {
+            e.printStackTrace();
+            return ResponseEntity.notFound().build();
+        }
     }
+
+
 
     /**
      * Liste tous les covoiturages enregistrés dans le système
@@ -263,4 +215,70 @@ public class CovoiturageController {
 
         covoiturageService.testFindPassager(covoitId);
     }*/
+
+
+
+    /* Fonctions utilitaires */
+    public static Covoiturage Covoiturage_instanceExample() {
+        Covoiturage covoiturage = new Covoiturage();
+        Commune commune = new Commune("Paris", 75000);
+        Adresse adresseDepart = new Adresse(26, "rue des Alouettes", commune);
+        Adresse adresseArrivee = new Adresse(32, "Bvd des Aubépines", commune);
+
+        Utilisateur conducteur = new Utilisateur();
+        LocalDate dateConducteur = LocalDate.of(2022, 4, 6);
+        List<Covoiturage> conducteurCovoituragesOrganises = new ArrayList<>();
+        List<Covoiturage> conducteurCovoituragesPassagers = new ArrayList<>();
+        List<String> roleConducteur = new ArrayList<>();
+        roleConducteur.add("COLLABORATEUR");
+        conducteur.setEmail("RonaldMerziner@gmail.com");
+        conducteur.setMotDePasse("4321");
+        conducteur.setNom("Merziner");
+        conducteur.setCovoituragesOrganises(conducteurCovoituragesOrganises);
+        conducteur.setCovoituragesPassagers(conducteurCovoituragesPassagers);
+        conducteur.setDateNonValide(dateConducteur);
+        conducteur.setRoles(roleConducteur);
+
+        Utilisateur passager = new Utilisateur();
+        LocalDate datePassager = LocalDate.of(2020, 1, 8);
+        List<Covoiturage> covoituragesOrganises = new ArrayList<>();
+        List<Covoiturage> covoituragesPassagers = new ArrayList<>();
+        List<String> rolePassager = new ArrayList<>();
+        rolePassager.add("COLLABORATEUR");
+        passager.setEmail("RonaldMerziner@gmail.com");
+        passager.setMotDePasse("4321");
+        passager.setNom("Merziner");
+        passager.setCovoituragesOrganises(covoituragesOrganises);
+        passager.setCovoituragesPassagers(covoituragesPassagers);
+        passager.setDateNonValide(datePassager);
+        passager.setRoles(rolePassager);
+        List<Utilisateur> passagersABord = new ArrayList<>();
+
+        /* A CORRIGER : CET APPEL POSE PROBLEME */
+        // passagersABord.add(conducteur);
+
+        VehiculePerso vehiculeConducteur = new VehiculePerso();
+        vehiculeConducteur.setImmatriculation("789-hu-78");
+        Modele modele = new Modele();
+        Marque marque = new Marque();
+        marque.setNom("Fiat");
+        modele.setNom("mini500");
+        modele.setMarque(marque);
+        vehiculeConducteur.setModele(modele);
+        vehiculeConducteur.setNombreDePlaceDisponibles(4);
+        vehiculeConducteur.setProprietaire(conducteur);
+
+        covoiturage.setDistanceKm(15);
+        covoiturage.setDureeTrajet(30);
+        covoiturage.setNombrePlacesRestantes(4);
+        covoiturage.setAdresseDepart(adresseDepart);
+        covoiturage.setAdresseArrivee(adresseArrivee);
+        covoiturage.setOrganisateur(conducteur);
+        covoiturage.setId(23);
+        covoiturage.setVehiculePerso(vehiculeConducteur);
+        covoiturage.setPassagers(passagersABord);
+
+        return covoiturage;
+    }
+
 }
